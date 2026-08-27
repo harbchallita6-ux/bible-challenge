03765baf8ea1367b089bd29591b21dc9bc599185
harbchallita6-ux <harbchallita6@gmail.com>
2026-08-27 11:30:35 +0300

    Create questions.js

---
+++ index.html<!DOCTYPE html> <html lang="en"> <head>   <meta charset="UTF-8">   <meta name="viewport" content="width=device-width, initial-scale=1.0">   <title>Bible Challenge | تحدّي الكتاب المقدس</title> </head>  <body>    <h1>Bible Challenge</h1>   <h2>تحدّي الكتاب المقدس</h2>    <p>Choose your language / اختر لغتك</p>    <button>العربية 🇱🇧</button>   <button>English 🇺🇸</button>  </body> </questions.js
@@ -0,0 +1,27 @@
+const questions = [
+  {
+    ar: "من بنى الفلك؟",
+    en: "Who built the ark?",
+    answers: [
+      "إبراهيم — Abraham",
+      "موسى — Moses",
+      "نوح — Noah",
+      "داود — David"
+    ],
+    correct: 2,
+    level: "Easy"
+  },
+
+  {
+    ar: "من هزم جليات؟",
+    en: "Who defeated Goliath?",
+    answers: [
+      "شاول — Saul",
+      "داود — David",
+      "صموئيل — Samuel",
+      "سليمان — Solomon"
+    ],
+    correct: 1,
+    level: "Easy"
+  }
+];
